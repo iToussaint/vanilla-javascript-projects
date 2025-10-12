df55fcc5a2f6cf6e663fa56d0ef2bd14371f6122
iToussaint <iradukundatoussaint01@gmail.com>
2025-10-12 16:21:31 +0200

    Complete the weight converter project

--- weight-converter/src/main.js
+++ weight-converter/src/main.js
@@ -1,11 +1,10 @@
-const pounds = document.getElementById("pounds")
-const grams = document.getElementById("grams")
-const kilograms = document.getElementById("kilograms")
-const ounces = document.getElementById("ounces")
+const pounds = document.getElementById("pounds");
+const grams = document.getElementById("grams");
+const kilograms = document.getElementById("kilograms");
+const ounces = document.getElementById("ounces");
 
-
-pounds.addEventListener("input", function(){
-    grams.textContent = this.value/0.45359237
-    kilograms
-    ounces
-})
+pounds.addEventListener("input", function () {
+  grams.textContent = this.value / 453.59237;
+  kilograms.textContent = this.value / 0.45359237;
+  ounces.textContent = this.value / 16;
+});
