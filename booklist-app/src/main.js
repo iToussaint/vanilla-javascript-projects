a14f965baae517c782b3d067624018e8f69bcdbb
iToussaint <iradukundatoussaint01@gmail.com>
2025-10-12 15:29:39 +0200

    Complete BookList App

--- booklist-app/src/main.js
+++ booklist-app/src/main.js
@@ -1,17 +1,18 @@
 const table = document.querySelector("table");
-const rows = table.querySelector("tbody")
+const rows = table.querySelector("tbody");
 const form = document.querySelector("form");
 const bookTitle = document.getElementById("title");
 const bookAuthor = document.getElementById("author");
 const bookISBN = document.getElementById("isbn");
 const addMessage = document.getElementById("add-message");
 const deleteMessage = document.getElementById("delete-message");
+const emptyFieldsMessage = document.getElementById("empty-fields-message");
+const alreadyExistMessage = document.getElementById("already-exist-message");
 let books = new Array();
 
+console.log(table.querySelector("tbody"));
 
-console.log(table.querySelector("tbody"))
-
-console.log("hey   "+localStorage.hey)
+console.log("hey   " + localStorage.hey);
 
 class Book {
   constructor(title, author, isbn) {
@@ -25,12 +26,14 @@ class UI {
   static createRow(book) {
     const newRow = rows.insertRow();
     const cellTitle = newRow.insertCell();
+    cellTitle.classList.add("p-3");
     cellTitle.textContent = book.title;
     const cellAuthor = newRow.insertCell();
     cellAuthor.textContent = book.author;
     const cellISBN = newRow.insertCell();
     cellISBN.textContent = book.isbn;
     const cellBtn = newRow.insertCell();
+
     cellBtn.innerHTML =
       "<button class='bg-red-500 rounded-sm text-slate-50 size-6 cursor-pointer'>X</button>";
   }
@@ -43,21 +46,29 @@ class UI {
   }
 }
 
+document.onload = UI.table(JSON.parse(localStorage.books));
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   if (localStorage.books === undefined) books = [];
   else books = JSON.parse(localStorage.books);
 
-  if (books.some((book) => book.isbn === bookISBN.value))
-    console.log("message");
-  else if (
+  if (books.some((book) => book.isbn === bookISBN.value)) {
+    alreadyExistMessage.classList.remove("hidden");
+    setTimeout(() => {
+      alreadyExistMessage.classList.add("hidden");
+    }, 2000);
+  } else if (
     bookTitle.value.trim() === "" ||
     bookAuthor.value.trim() === "" ||
     bookISBN.value.trim() === ""
-  )
-    console.log("message");
-  else {
+  ) {
+    emptyFieldsMessage.classList.remove("hidden");
+    setTimeout(() => {
+      emptyFieldsMessage.classList.add("hidden");
+    }, 2000);
+  } else {
     books.push(new Book(bookTitle.value, bookAuthor.value, bookISBN.value));
     localStorage.setItem("books", JSON.stringify(books));
     addMessage.classList.remove("hidden");
